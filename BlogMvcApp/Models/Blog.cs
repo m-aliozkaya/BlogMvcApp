ca14f41d2d2f0667b556559e92a74943ce519a10
Muhammed Ali OZKAYA <m_aliozkaya@hotmail.com>
2021-08-18 17:01:11 +0300

    Category and blog models added.

---
+++ BlogMvcApp/Models/Blog.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BlogMvcApp.Models
+{
+    public class Blog
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public string Content { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public bool IsValid { get; set; }
+        public bool IsActive { get; set; }
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
